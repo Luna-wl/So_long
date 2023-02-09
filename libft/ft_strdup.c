e4c26eee7e323bb97906210a335379679fb49de5
Warintorn Luedara <wluedara@z3t4c4.42bangkok.com>
2023-02-09 17:19:01 +0700

    my lovely libft

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: wluedara <Warintorn_L@outlook.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 23:28:11 by wluedara          #+#    #+#             */
-/*   Updated: 2023/02/04 15:50:45 by wluedara         ###   ########.fr       */
+/*   Updated: 2023/02/08 18:05:28 by wluedara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,17 @@
 char	*ft_strdup(char *s1)
 {
 	char	*dest;
-	char	*str;
 	int		len;
 	int		i;
 
-	str = (char *)s1;
-	len = ft_strlen(str);
+	len = ft_strlen(s1);
 	dest = (char *)malloc(sizeof(char) * len + 1);
 	if (!dest)
 		return (0);
 	i = 0;
 	while (i < len)
 	{
-		dest[i] = str[i];
+		dest[i] = s1[i];
 		i++;
 	}
 	dest[i] = '\0';
